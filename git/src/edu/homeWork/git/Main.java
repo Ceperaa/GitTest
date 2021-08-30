32d67d51e9c63aa4187f67385055f8b1626c0154
”Ivan <cepperaa@gmail.com>
2021-08-30 17:22:53 +0300

    fix1

--- git/src/edu/homeWork/git/Main.java
+++ git/src/edu/homeWork/git/Main.java
@@ -3,8 +3,6 @@ package edu.homeWork.git;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("1");
-        System.out.println("2");
+
     }
 }
