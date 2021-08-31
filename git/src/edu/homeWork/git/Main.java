b8f5fb61056ae15977f66bc0f7f596c1ed91a0d9
”Ivan <cepperaa@gmail.com>
2021-08-31 21:33:20 +0300

    new

--- git/src/edu/homeWork/git/Main.java
+++ git/src/edu/homeWork/git/Main.java
@@ -3,6 +3,9 @@ package edu.homeWork.git;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("hello world");
+        System.out.println("1");
+        System.out.println("2");
     }
 }
+ 
